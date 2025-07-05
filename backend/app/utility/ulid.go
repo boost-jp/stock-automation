fbb2879d3dc190203650e74c65d453d36e12e382
Yuta Tasaka <yuta.tasaka@gmail.com>
2025-07-05 15:12:24 +0900

    fix: Add missing utility package with ULID generation

---
+++ backend/app/utility/ulid.go
@@ -0,0 +1,15 @@
+package utility
+
+import (
+	"crypto/rand"
+	"time"
+
+	"github.com/oklog/ulid/v2"
+)
+
+// NewULID generates a new ULID string.
+func NewULID() string {
+	t := time.Now()
+	entropy := ulid.Monotonic(rand.Reader, 0)
+	return ulid.MustNew(ulid.Timestamp(t), entropy).String()
+}
